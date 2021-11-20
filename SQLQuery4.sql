454daaee048729938fd14fa62dcac522a7fd34fd
dharshini12300 <94680879+dharshini12300@users.noreply.github.com>
2021-11-20 13:58:31 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,14 @@
+CREATE TABLE employee  
+(  
+employee_id (10, 0),  
+first_Name varchar(25),  
+last_Name varchar(25),  
+email varchar(25),  
+phone_number varchar(25),  
+hire_date varchar(25),
+jod_id varchar (25),
+salary varchar (25),
+commission varchar (25),
+manager_id varchar (25),
+department_id varchar (25)
+); 
